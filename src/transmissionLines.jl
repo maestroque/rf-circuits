b9125e2ff5dc67f1b8c91386ef2285387c798b18
GeorgeKikas <kikasgeorg@ece.auth.gr>
2023-04-17 14:18:34 +0300

    Create fitting function for variable parameters circuit optimization for reflection coefficient

--- src/transmissionLines.jl
+++ src/transmissionLines.jl
@@ -1,10 +1,11 @@
-using Plots
 
-#=
+"""
 z0: Characteristic Impedance in Ω
 zLoad: Load's Impedance Ω
 length: Transmission Line Length measured in fraction of λ
-=#
+
+Returns the input impedance of the transmission line
+"""
 function zIn(z0, zLoad, length)
     if length == 0.25
         return z0 ^ 2 / zLoad
@@ -13,28 +14,11 @@ function zIn(z0, zLoad, length)
     end
 end
 
-function circuitReflection(f)
-    z0 = 50
-    loadLine = 0.2 * f / f0
-    shortLine = 0.13 * f / f0
-    connectingLine = 0.1 * f / f0
+"""
+z1, z2: Impedance values in parallel
 
-    z1 = zIn(z0, 100 - 79.57im, loadLine) / z0
-    zShort = zIn(z0, 0, shortLine) / z0
-    z2 = 1 / (1 / z1 + 1 / zShort)
-    zIn2 = zIn(z0, z2 * z0, connectingLine) / z0
-    zLoadTotal = 1 / zIn2 + 2π * 2.7 * 10e-3 * 50
-    return (zLoadTotal - 50) / (zLoadTotal + 50)
-end
-
-N = 201
-f0 = 1e9
-z0 = 50
-
-# Frequencies to be simulated
-f = 0 : (4 * f0 / N) : (4 * f0)
-
-# Reflection Coefficients for the frequency sweep
-Γ = circuitReflection.(f)
-
-plot(f, 20log10.(abs.(Γ)))
+Returns the equivalent parallel impedance value
+"""
+function parallel(z1, z2)
+    return 1 / (1 / z1 + 1 / z2)
+end    
