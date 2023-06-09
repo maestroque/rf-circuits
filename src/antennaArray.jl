f210e57a709bd06cb4a2d78adb6654b13a04369c
GeorgeKikas <kikasgeorg@ece.auth.gr>
2023-06-09 16:44:19 +0300

    Add AntennaArray struct

--- src/antennaArray.jl
+++ src/antennaArray.jl
@@ -3,6 +3,31 @@ using Plots
 using PyPlot
 using PyCall
 
+mutable struct AntennaArray
+    λ::Float64
+    I::Float64
+    d::Float64
+    l::Float64
+    N::Int
+    alternatingCurrents::Bool
+
+    """
+    ## Arguements
+    - `f`: Frequency
+    - `I`: Current 
+    - `dλ`: Distance between dipoles (fraction of λ)
+    - `lλ`: Length of the dipole (fraction of λ)
+    - `N`: Number of dipoles
+    - `alternatingCurrents`: Whether all currents are the same polarity or alternating
+    """
+    function AntennaArray(f, I, dλ, lλ, N = 1, alternatingCurrents = false)
+        λ = 3e8 / f
+        d = dλ * λ
+        l = lλ * λ
+        new(λ, I, d, l, N, alternatingCurrents)
+    end
+end
+
 """
 ## Arguements
 - `f`: Frequency
@@ -12,23 +37,20 @@ using PyCall
 - `ϕ`:
 - `N`: Number of dipoles
 - `alternatingCurrents`: Whether all currents are the same polarity or alternating
-
-
 """
-function antennaArrayFieldIntensity(f, I, d, l, θ, ϕ, N, alternatingCurrents=false)
-    λ = 3e8 / f
-    k = 2π / λ
+function antennaArrayFieldIntensity(a::AntennaArray, θ, ϕ)
+    k = 2π / a.λ
     r = 10
     arrayFactor = 1
-    for i in range(1, N-1)
-        if alternatingCurrents
-            arrayFactor += (-1)^(i) * exp(im*k*d*(i)*cos(ϕ)*sin(θ))
+    for i in range(1, a.N-1)
+        if a.alternatingCurrents
+            arrayFactor += (-1)^(i) * exp(im * k * a.d * (i) * cos(ϕ) * sin(θ))
         else
-            arrayFactor += exp(im*k*d*(i+1)*cos(ϕ)*sin(θ))
+            arrayFactor += exp(im * k * a.d * (i + 1) * cos(ϕ) * sin(θ))
         end 
     end
 
-    return abs(im*60*I*exp(-im*k*r)*(cos(k/2 * l * cos(θ)) - cos(k/2 * l)) / (r * sin(θ)) * arrayFactor)
+    return abs(im * 60 * a.I * exp(-im * k * r) * (cos(k / 2 * a.l * cos(θ)) - cos(k / 2 * a.l)) / (r * sin(θ)) * arrayFactor)
 end
 
 function color_function(x, y, z)
@@ -37,20 +59,18 @@ function color_function(x, y, z)
 end
 
 maxSteps = 100
+f = 1e9
+I = 1
 θ = range(0, stop=2π, length=maxSteps)
 ϕ = range(0, stop=π, length=maxSteps)
-f = 1e9
-λ = 3e8 / f
-N = 8
-d = λ/8
-l = λ/2
-alternating = true
+a = AntennaArray(f, I, 0.5, 0.5)
 
-E = antennaArrayFieldIntensity.(1e9, 1, d, l, 0, ϕ, N)
+
+# E = antennaArrayFieldIntensity.(Ref(a), 0, ϕ)
 gr()
 
-display(Plots.plot(ϕ, antennaArrayFieldIntensity.(1e9, 1, d, l, π/2, ϕ, N, alternating), proj=:polar, gridlinewidth=2, title="θ=0"))
-display(Plots.plot(θ, antennaArrayFieldIntensity.(1e9, 1, d, l, θ, 0, N, alternating), proj=:polar, gridlinewidth=2, title="φ=0"))
+display(Plots.plot(ϕ, antennaArrayFieldIntensity.(Ref(a), π/2, ϕ), proj=:polar, gridlinewidth=2, title="θ=0"))
+display(Plots.plot(θ, antennaArrayFieldIntensity.(Ref(a), θ, 0), proj=:polar, gridlinewidth=2, title="φ=0"))
 
 X(r,theta,phi) = r * sin(theta) * sin(phi)
 Y(r,theta,phi) = r * sin(theta) * cos(phi)
@@ -58,9 +78,9 @@ Z(r,theta,phi) = r * cos(theta)
 
 pyplot()
 
-xs = [X(antennaArrayFieldIntensity(1e9, 1, d, l, theta, phi, N, alternating), theta, phi) for theta in θ, phi in ϕ]
-ys = [Y(antennaArrayFieldIntensity(1e9, 1, d, l, theta, phi, N, alternating), theta, phi) for theta in θ, phi in ϕ]
-zs = [Z(antennaArrayFieldIntensity(1e9, 1, d, l, theta, phi, N, alternating), theta, phi) for theta in θ, phi in ϕ]
+xs = [X(antennaArrayFieldIntensity(a, theta, phi), theta, phi) for theta in θ, phi in ϕ]
+ys = [Y(antennaArrayFieldIntensity(a, theta, phi), theta, phi) for theta in θ, phi in ϕ]
+zs = [Z(antennaArrayFieldIntensity(a, theta, phi), theta, phi) for theta in θ, phi in ϕ]
 
 # Initiate the radial color map
 R = color_function.(xs, ys, zs)
